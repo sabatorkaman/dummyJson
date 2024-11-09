ab411e0a0cd3fdd160c73a08eea7dbe9e9b94d83
saba <s.torkaman.kh@gmail.com>
2024-11-09 23:02:26 +0330

    add posts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -139,37 +139,6 @@ export interface Company {
     country: string
   }
 }
-// export interface Newusers {
-
-//   id: number,
-//   username: string,
-//   email: string,
-//   firstName: string,
-//   lastName: string,
-//   gender: string,
-//   image: string,
-//   accessToken: string, // JWT accessToken (for backward compatibility) in response and cookies
-//   refreshToken: string // refreshToken in response and cookies
-
-// }
-// export interface User {
-//   id: number,
-//   firstName: string,
-//   lastName: string,
-//   maidenName: string,
-//   age: number,
-//   gender: string,
-//   email: string,
-//   phone: string,
-//   username: string,
-//   password: string,
-//   birthDate: string,
-//   image: string,
-//   bloodGroup: string,
-//   height: number,
-//   weight: number,
-//   eyeColor: string,
-// }
 
 export interface AddNewUser {
 
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,20 +1,68 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
+import { Component, inject, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { AllPostDetails, ApiService, PostDetail } from '../api.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
 export class PostComponent implements OnInit {
   readonly api = inject(ApiService)
+  postData?: AllPostDetails
+  limit=1
+  skip?: number
+  avator?: string
+  like=20
 
   ngOnInit(): void {
-    this.api.getPost(1, 3).subscribe((data) => {
+    this.api.getPost(this.limit, this.skip ?? 0).subscribe((data) => {
       console.log(data)
+      this.postData = data
     })
+    this.api.getAllUser(this.limit ?? 0, this.skip ?? 0, "").subscribe((users) => {
+      console.log(users)
+      users.users.map((item) => {
+        this.avator = item.image
+      })
+    })
+  
   }
+  morPost(){
+      console.log(this.limit)
+      this.limit+=1
+      console.log(this.limit)
+      this.api.getPost(this.limit??0, this.skip??0).subscribe((data) => {
+        console.log(data)
+        this.postData = data
+      })
+  }
+  clickLike(){
+    // this.api.getPost(this.limit ?? 0 ,this.skip ?? 0 ).subscribe((item)=>{
+    //   item.posts.map((item=>{
+    //     console.log(item.reactions.likes)
+    //     this.like=item.reactions.likes
+    //     this.like +=1
+    //     console.log(this.like)
+    //   }))
+    // })
+    // this.postData?.posts.map((item)=>{
+    //   console.log(item.reactions.likes + 1) 
+    // })
 
+      this.api.getPost(this.limit ?? 0 ,this.skip ?? 0 ).subscribe((item)=>{
+      // item.posts.map((item=>{
+      //   console.log(item.reactions.likes)
+      //   this.like=item.reactions.likes
+      //   this.like +=1
+      //   console.log(this.like+=1)
+      // }))
+    })
+    this.like+=1
+    console.log(this.like)
+  }
 }
